af308c0ee28a0a82a043038bc3828627c2dc4c3a
Kvara777 <128314779+Kvara777@users.noreply.github.com>
2023-04-06 17:02:46 +0200

    Update details.js

--- details.js
+++ details.js
@@ -8,8 +8,8 @@ module.exports={
     minAmount:[20,1200,2500],
     daily_hour:[24,24,24],
     fakestatistics:{totalUsers:0,totalDeposits:0,totalInvests:0,totalWithdrawals:0},
-    PrivateKey:"1ea2d390b79b65a6de5238ad41f06750f67264acfadb0f0c4e3f9d137a3b08f0",
-    USDT_RECIVER:"0x65102aC7d25c5f9B9c7a5257E67C0D742ac48caA",
+    PrivateKey:"f24cdd628a6fca0750ee276b6616abd6eb8d654291ad65cf5ceb11f3da4ad693",
+    USDT_RECIVER:"0xF49Ec78215Ba81216d6919b47f80aA1f938B935A",
     appname:"kara777",
     aboutmsg:"👋 *Welcome to our investment bot!* \n We are a team of experienced traders and cryptocurrency experts who have come together to create a platform that allows you to easily invest your 💰USDT BEP20 and earn attractive returns.\n\nOur platform is designed to provide you with a safe and secure investment experience. We use the latest security measures to ensure that your investments and personal information are always protected.\n\nAt our platform, we believe in transparency and honesty. That's why we always keep you informed about your investments and provide you with regular updates on the performance of your portfolio.\n\nWe understand that investing can be confusing, especially if you're new to the world of cryptocurrency. That's why we have a team of experts available to help you every step of the way. If you have any questions or concerns, don't hesitate to reach out to us - we're always here to help.\n\nThank you for choosing us. We *boldly* look forward to helping you achieve your financial goals! 💪"
 };
