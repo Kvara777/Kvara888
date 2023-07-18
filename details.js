d79398f31cf6c324abb62029ad448adbad955bfd
Kvara777 <128314779+Kvara777@users.noreply.github.com>
2023-07-18 19:22:54 +0200

    Update details.js

--- details.js
+++ details.js
@@ -10,5 +10,5 @@ module.exports={
     fakestatistics:{totalUsers:0,totalDeposits:0,totalInvests:0,totalWithdrawals:0},
     PrivateKey:"0x23d8293538ddc0155776b88e4e50f88cdc60d8492d08036c7497ea61c937de68",
 Address:"0xF49Ec78215Ba81216d6919b47f80aA1f938B935A",
-    appname:"https://bozip.onreder.com",
+    appname:"https://kvara777.onrender.com",
     aboutmsg:"👋 Welcome to our investment platform! \n We are a team of financial experts and investment professionals who have created a platform to make investing in cryptocurrencies easy and profitable for you. 💰💹\n\nOur platform ensures the safety and security of your investments and personal information, utilizing the latest security measures. We believe in honesty and transparency, providing regular updates and information on your portfolio's performance. 📊\n\nInvesting can be complex, especially in the world of cryptocurrencies. That's why we have a team of knowledgeable experts available to support you at every stage of your investment journey. We welcome all questions and concerns and are dedicated to helping you succeed in your financial goals. 💪\n\nThank you for choosing us. We are excited to embark on this investment journey with you! 🚀"}
